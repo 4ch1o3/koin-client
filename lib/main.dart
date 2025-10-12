8459ee551e066bfec38482801319991abac251bc
wannabe03 <qaz73700@naver.com>
2025-10-13 02:04:59 +0900

    chore: import project as a fresh repo

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:koin/user/views/home_screen.dart';
+import 'package:koin/user/views/auth/views/splash_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Pretendard',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenWrapper(),
+    )
+  );
+}
